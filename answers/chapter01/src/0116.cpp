8312fe38b3c29a6a79d7b90c7d907bf0b0bd8e51
erik <amaduesmozart@outlook.com>
2020-03-14 01:53:29 +0800

    20200314---第一章完结

--- answers/chapter01/src/0116.cpp
+++ answers/chapter01/src/0116.cpp
@@ -10,3 +10,20 @@ using std::cin;
 using std::endl;
 using std::vector;
 
+int main()
+{
+    vector<int> ivec;
+    int elem;
+    int sum;
+
+    cout<<"Enter integers,q to quit"<<endl;
+
+    while (cin>>elem && elem!='q')
+        ivec.push_back(elem);
+
+    vector<int>::iterator iter;
+    for(iter=ivec.begin();iter!=ivec.end();++iter)
+        sum+=*iter;
+    cout<<"sum is "<<sum<<endl;
+    return 0;
+}
